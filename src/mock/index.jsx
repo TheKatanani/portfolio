1c0813823ba80022858b4c468eb243b035badcdf
Abu khaled <mkhkatanany@gmail.com>
2023-08-01 02:10:31 +0300

    make the project ready to deploy

--- src/mock/index.jsx
+++ src/mock/index.jsx
@@ -1,8 +1,8 @@
 // images 
 import upwork from '../assets/images/Upwork.png'
-// import brand from '../assets/images/brand.png'
-// import game from '../assets/images/game.png'
-// import resturant from '../assets/images/resturant.png'
+import brand from '../assets/images/brand.png'
+import game from '../assets/images/game.png'
+import resturant from '../assets/images/resturant.png'
 import test from '../assets/images/Screenshot 2023-07-28 145654.png'
 import test2 from '../assets/images/Screenshot 2023-07-28 160436.png'
 import test3 from '../assets/images/Screenshot 2023-07-28 171838.png'
@@ -27,6 +27,7 @@ export const MenuItems =[
 export const porjects =[
   {
     img:test3,
+    img2:upwork,
     skills:["json-server","styled-component","react","redux","authantication"],
     title:"Upwork Clone", 
     subTitle:"Freelance Platform",
@@ -36,6 +37,7 @@ export const porjects =[
   },
   {
     img:test2,
+    img2:brand,
     skills:["two Themes","styled-component","react","responsive"],
     title:"Brand(AliExpress clone)", 
     subTitle:"e-commerce website",
@@ -45,6 +47,7 @@ export const porjects =[
   },
   {
     img:test4,
+    img2:game,
     skills:["authantication","class coomponents","react"],
     title:"PS-Game", 
     discription:["PS-GameP1", "PS-GameP2", "PS-GameP3","PS-GameP4"] ,
@@ -53,6 +56,7 @@ export const porjects =[
   },
   {
     img:test,
+    img2:resturant, 
     skills:["responsive","class coomponents","react"],
     title:"Vegan Restaurant Website", 
     discription:["RestaurantP1", "RestaurantP2"] ,
